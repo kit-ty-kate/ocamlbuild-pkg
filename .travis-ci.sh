cdcdfc5bec8a4101e0ba8a452c59bb8b506f2d22
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2016-11-16 19:37:31 +0100

    Improve travis (CI) script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,15 +1,28 @@
-# Install OPAM
-wget https://raw.github.com/ocaml/opam/master/shell/opam_installer.sh -O - | sudo sh -s /usr/local/bin
+# Init OPAM bin directory
+mkdir -p $HOME/opam-bin
+export PATH="$HOME/opam-bin:$PATH"
 
-# Init OPAM
-opam init -y --compiler=$OCAML_VERSION
+# Install & Init OPAM if not cached
+if type opam; then
+    cp -r $HOME/opam-cache $HOME/.opam
+else
+    # Install & Init OPAM
+    wget "https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux" -O $HOME/opam-bin/opam
+    chmod u+x $HOME/opam-bin/opam
+    opam init -y --compiler=$OCAML_VERSION
+
+    # Save for cache
+    cp -r $HOME/.opam $HOME/opam-cache
+fi
+
 eval `opam config env`
 
 # Check OPAM package description
 opam lint
 
 # Install
-opam pin add -y --kind=git ocamlbuild-pkg .
+opam pin add -y --no-action --kind=git ocamlbuild-pkg .
+opam install -y ocamlbuild-pkg
 
 # Run tests
 make examples
